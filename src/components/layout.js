bcd47515a4f6de8a8b832c4ed5750fa89a54f8d2
rhaiyunwrites <ryan.ngoy@gmail.com>
2023-01-26 19:59:53 -0800

    fixed typo

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Header from "../components/Header"
+import Header from "../components/header"
 import Navbar from "../components/navbar"
 import Content from "../components/content.module.css"
 import layoutStyles from "../components/layout.module.css"
