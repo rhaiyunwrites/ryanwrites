3dfe754082bada9bef9c845dce0b3b3bdbf0ca17
rhaiyunwrites <ryan.ngoy@gmail.com>
2023-02-15 07:02:44 -0800

    edited home page text

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,10 @@ const IndexPage = () => (
         Please feel free to use the material on this site as a resource for your studies. I hope it can help you 
         overcome some of the many struggles I faced when I first encountered these subjects.
       </p>
+      <p>
+      	If you find any errors, mistakes, omissions, or blatant lies anywhere on this website, please reach out 
+      	and let me know! I greatly appreciate the feedback and will be sure to give you the proper credit due!
+      </p>
   </Layout>
 )
 
